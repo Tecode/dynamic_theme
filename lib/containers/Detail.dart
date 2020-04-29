8e4d4fc35fa60a5b42eef6221d8c8f5c023ecf9e
Tecode <283731869@qq.com>
2020-04-29 19:25:58 +0800

    feat: 修改弹窗颜色

--- lib/containers/Detail.dart
+++ lib/containers/Detail.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:dynamic_theme/widgets/common/AlertDialog.dart';
+import 'package:flutter/cupertino.dart'
+    hide CupertinoAlertDialog, CupertinoDialogAction;
 import 'package:flutter/material.dart';
 
 class Detail extends StatelessWidget {
@@ -6,6 +8,41 @@ class Detail extends StatelessWidget {
   static String routeName = '/detail';
 
   const Detail({this.value});
+
+  void showDialog({BuildContext context, Widget child}) {
+    showCupertinoDialog<String>(
+      context: context,
+      builder: (BuildContext context) => child,
+    ).then((String value) {
+//      if (value != null) {
+//        setState(() { lastSelectedValue = value; });
+//      }
+    });
+  }
+
+  void _onAlertWithTitlePress(BuildContext context) {
+    showDialog(
+      context: context,
+      child: CupertinoAlertDialog(
+        title: const Text(
+            'Allow "Maps" to access your location while you are using the app?'),
+        content: const Text(
+            'Your current location will be displayed on the map and used '
+            'for directions, nearby search results, and estimated travel times.'),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            child: const Text('Don\'t Allow'),
+            onPressed: () => Navigator.pop(context, 'Disallow'),
+          ),
+          CupertinoDialogAction(
+            child: const Text('Allow'),
+            onPressed: () => Navigator.pop(context, 'Allow'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final Detail arguments = ModalRoute.of(context).settings.arguments;
@@ -13,11 +50,27 @@ class Detail extends StatelessWidget {
       navigationBar: CupertinoNavigationBar(
         middle: Text(arguments.value),
       ),
-      child: Material(
-        child: Center(
-          child: Text(
-            arguments.value,
-            style: Theme.of(context).textTheme.display3,
+      child: SafeArea(
+        child: Material(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('TITLE', style: Theme.of(context).textTheme.display3),
+                CupertinoButton(
+                  padding: EdgeInsets.zero,
+                  child: Text('弹窗'),
+                  onPressed: () => _onAlertWithTitlePress(context),
+                ),
+                CupertinoButton(
+                  padding: EdgeInsets.zero,
+                  child: Text('底部弹窗'),
+                  onPressed: () => {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
