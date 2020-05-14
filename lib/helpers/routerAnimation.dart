7dd3b4249f907666e692a23cacd05c6e0bcf482e
Tecode <283731869@qq.com>
2020-05-14 16:27:46 +0800

    feat: 自定义路由动画
    底部弹出路由动画

---
+++ lib/helpers/routerAnimation.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+Route bottomPopRouter(Widget widget) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => widget,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
