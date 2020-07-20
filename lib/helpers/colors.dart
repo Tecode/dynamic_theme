a963666b8834b1adf81100394b91d40495132f74
Tecode <283731869@qq.com>
2020-07-20 16:09:25 +0800

    修改静态方法为命名构造函数

--- lib/helpers/colors.dart
+++ lib/helpers/colors.dart
@@ -1,44 +1,30 @@
 import 'package:flutter/material.dart';
 
-@immutable
 class ColorTheme {
-  /// 边框颜色
-  final Color borderColor;
-  final Color cubeColor;
-  final Color activeNavColor;
-  final Color navBarColor;
-  final Color colorF3F3F6;
-  final Color color202326;
+  Color borderColor;
+  Color cubeColor;
+  Color activeNavColor;
+  Color navBarColor;
+  Color colorF3F3F6;
+  Color color202326;
 
-  const ColorTheme({
-    this.borderColor,
-    this.cubeColor,
-    this.activeNavColor,
-    this.navBarColor,
-    this.colorF3F3F6,
-    this.color202326,
-  });
-
-  static ColorTheme of(BuildContext context) {
+  ColorTheme.of(BuildContext context) {
 //  暗黑色
     if (Theme.of(context).brightness == Brightness.dark) {
-      return const ColorTheme(
-        borderColor: Color(0xfff161617),
-        cubeColor: Colors.white70,
-        activeNavColor: Colors.brown,
-        navBarColor: Color(0xff161616),
-        colorF3F3F6: Color(0xff18191b),
-        color202326: Color(0xff4e5156),
-      );
+      borderColor = Color(0xfff161617);
+      cubeColor = Colors.white70;
+      activeNavColor = Colors.brown;
+      navBarColor = Color(0xff161616);
+      colorF3F3F6 = Color(0xff18191b);
+      color202326 = Color(0xff4e5156);
+      return;
     }
 //    明亮色
-    return ColorTheme(
-      borderColor: Color(0xffdedede),
-      cubeColor: Colors.black38,
-      activeNavColor: Colors.amberAccent,
-      navBarColor: Colors.white,
-      colorF3F3F6: Color(0xffF3F3F6),
-      color202326: Color(0xff202326),
-    );
+    borderColor = Color(0xffdedede);
+    cubeColor = Colors.black38;
+    activeNavColor = Colors.amberAccent;
+    navBarColor = Colors.white;
+    colorF3F3F6 = Color(0xffF3F3F6);
+    color202326 = Color(0xff202326);
   }
 }
