6a9aef523bf9802fbffae89780c8239335f07039
Tecode <283731869@qq.com>
2021-07-31 14:06:57 +0800

    鍵盤彈起固定輸入框

--- lib/containers/chat_list.dart
+++ lib/containers/chat_list.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
-import 'package:dynamic_theme/containers/app.dart';
 import 'package:dynamic_theme/helpers/colors.dart';
-import 'package:dynamic_theme/widgets/common/dialog_box.dart';
+import 'package:dynamic_theme/helpers/textThemeStyle.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter/rendering.dart';
+// import 'package:keyboard_attachable/keyboard_attachable.dart';
 
 class ChatList extends StatefulWidget {
   static const String routeName = '/chatList';
@@ -18,6 +18,7 @@ class _ChatListState extends State<ChatList> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      // resizeToAvoidBottomInset: false,
       navigationBar: CupertinoNavigationBar(
         padding: EdgeInsetsDirectional.only(),
         transitionBetweenRoutes: Platform.isIOS,
@@ -37,63 +38,54 @@ class _ChatListState extends State<ChatList> {
       ),
       child: SafeArea(
         child: Material(
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return EasyRefresh.custom(
-                reverse: true,
-                slivers: <Widget>[
-//                  SliverList(
-//                    delegate: SliverChildBuilderDelegate(
-//                      (context, index) {
-//                        return Text('445');
-//                      },
-//                      childCount: 4,
-//                    ),
-//                  ),
-                  SliverToBoxAdapter(
-                    child: Container(
-                      height: constraints.maxHeight,
-                      width: double.infinity,
-                      child: Column(
-                        children: <Widget>[
-//                          for (MessageEntity entity in _msgList.reversed)
-//                            _buildMsg(entity),
-                          Container(
-                            color: Colors.deepOrangeAccent,
-                            height: 400,
-                            child: Text('8998'),
-                          ),
-                          TextButton(onPressed: () => showGeneralDialog(
-                            context: App.materialKey.currentContext!,
-                            barrierColor: Colors.black.withOpacity(0.5),
-                            barrierDismissible: true,
-                            barrierLabel: '',
-                            transitionDuration: Duration(milliseconds: 200),
-                            pageBuilder: (
-                                BuildContext buildContext,
-                                Animation<double> animation,
-                                Animation<double> secondaryAnimation,
-                                ) {
-                              return Builder(
-                                builder: (BuildContext context) => ScaleTransition(
-                                  scale: Tween<double>(begin: 0.5, end: 1.0).animate(
-                                    CurvedAnimation(
-                                      parent: animation,
-                                      curve: Curves.fastOutSlowIn,
-                                    ),
-                                  ),
-                                  child: DialogBox(),
-                                ),
-                              );
-                            },
-                          ), child: Text('全局弹出窗'))
-                        ],
-                      ),
+          child: Stack(
+            children: [
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+                onVerticalDragDown: (_) => FocusScope.of(context).requestFocus(FocusNode()),
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.only(bottom: 40.0),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(minHeight: MediaQuery.of(context).size.height),
+                    child: Column(
+                      children: [
+                        Text(
+                          '''KeyboardAttachable is a widget that adds space below its baseline when the soft keyboard is shown and hidden with an animation that matches that of the platform keyboard. This widget can be used to animate its child when the soft keyboard is shown or hidden, so that the child widget appears to be attached to the keyboard.
+You can combine these two widgets by passing a KeyboardAttachable widget as the footer of a FooterLayout to create a page that shrinks and expands when the soft keyboard appears and disappears, with an animation that matches the platform keyboard animation. Additionally, you can pass a child widget to your KeyboardAttachable to have a footer in your page that attaches itself to the soft keyboard when shown.
+In order for this to work with animations, the resizeToAvoidBottomInset parameter of the Scaffold above the page has to be false.
+''',
+                          style: TextThemeStyle.of(context).font16,
+                        ),
+                        Text(
+                          '''KeyboardAttachable is a widget that adds space below its baseline when the soft keyboard is shown and hidden with an animation that matches that of the platform keyboard. This widget can be used to animate its child when the soft keyboard is shown or hidden, so that the child widget appears to be attached to the keyboard.
+You can combine these two widgets by passing a KeyboardAttachable widget as the footer of a FooterLayout to create a page that shrinks and expands when the soft keyboard appears and disappears, with an animation that matches the platform keyboard animation. Additionally, you can pass a child widget to your KeyboardAttachable to have a footer in your page that attaches itself to the soft keyboard when shown.
+In order for this to work with animations, the resizeToAvoidBottomInset parameter of the Scaffold above the page has to be false.
+''',
+                          style: TextThemeStyle.of(context).font16,
+                        ),
+                        Text(
+                          '''KeyboardAttachable is a widget that adds space below its baseline when the soft keyboard is shown and hidden with an animation that matches that of the platform keyboard. This widget can be used to animate its child when the soft keyboard is shown or hidden, so that the child widget appears to be attached to the keyboard.
+You can combine these two widgets by passing a KeyboardAttachable widget as the footer of a FooterLayout to create a page that shrinks and expands when the soft keyboard appears and disappears, with an animation that matches the platform keyboard animation. Additionally, you can pass a child widget to your KeyboardAttachable to have a footer in your page that attaches itself to the soft keyboard when shown.
+In order for this to work with animations, the resizeToAvoidBottomInset parameter of the Scaffold above the page has to be false.
+''',
+                          style: TextThemeStyle.of(context).font16,
+                        ),
+                      ],
                     ),
-                  )
-                ],
-              );
-            },
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  color: ColorTheme.of(context).colorF3F3F6,
+                  child: TextField(),
+                ),
+              )
+            ],
           ),
         ),
       ),
